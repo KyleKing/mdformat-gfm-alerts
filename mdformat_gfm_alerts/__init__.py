78672fea4f270ba9919306dc39d06dd89b312521
Kyle King <KyleKing@users.noreply.github.com>
2025-05-21 19:27:02 -0600

    build: copier-auto-update

--- mdformat_gfm_alerts/__init__.py
+++ mdformat_gfm_alerts/__init__.py
@@ -2,6 +2,8 @@
 
 __version__ = "1.0.1"
 
+__plugin_name__ = "gfm_alerts"
+
 # FYI see source code for available interfaces:
 #   https://github.com/executablebooks/mdformat/blob/5d9b573ce33bae219087984dd148894c774f41d4/src/mdformat/plugins.py
 from .plugin import RENDERERS, update_mdit
